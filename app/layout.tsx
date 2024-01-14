29488218fe8f759fcfc5990f760b615dcfd4e4fc
Ernst Naezer <info@ernstnaezer.nl>
2024-01-14 21:50:38 +0100

    Fixed the og social image

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,14 @@ export default function RootLayout({
           href='/favicon-16x16.png'
         />
         <link rel='manifest' href='/site.webmanifest' />
+        <meta property='og:image' content='<generated>' />
         <meta
-          property='og:image'
-          content='/img/website_screenshot_2024-01-12T16-01-49.png'
+          property='og:image:alt'
+          content='Ontdek de wereld van de schietsport bij Schietsport Vereniging Zoetermeer: Een gemeenschap waar sport, gezelligheid en veiligheid centraal staan.'
         />
+        <meta property='og:image:type' content='image/png' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />{' '}
       </head>
       <body className={`${roboto.variable} ${montserrat.variable} bg-timeless`}>
         {children}
