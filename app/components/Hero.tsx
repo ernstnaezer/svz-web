05f1168f678cf05792ae6b39317cb3bb81b684ed
Ernst Naezer <p298892@DHGM121935.home>
2024-03-25 21:32:57 +0100

    new hero image

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,9 +5,16 @@ import heroImg from '../../public/img/hero.png';
 
 const Hero = () => {
   return (
-    <div className='w-full bg-eastern-blue pt-3'>
+    <div className='w-full bg-eastern-blue'>
       <div className='container flex flex-col items-center md:flex-row'>
-        <div className='flex-1 text-2xl text-white md:text-4xl lg:text-5xl  xl:text-6xl 2xl:text-7xl '>
+        <div className='flex flex-1 items-end justify-end'>
+          <Image
+            src={heroImg}
+            alt='De mensen van de vereniging'
+            className='relative bottom-[-52px] h-auto max-w-full'
+          />
+        </div>
+        <div className='flex-1 text-2xl text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl '>
           <h1 className='font-bold'>Wij zijn</h1>
           <h1 className='ml-4 font-bold lg:mb-4 lg:mt-2'>
             Schietsportvereniging
@@ -18,14 +25,6 @@ const Hero = () => {
               WORD LID
             </button>
           </Link>
-        </div>
-
-        <div className='flex flex-1 items-end justify-end'>
-          <Image
-            src={heroImg}
-            alt='De mensen van de vereniging'
-            className='h-auto max-w-full'
-          />
         </div>
       </div>
     </div>
