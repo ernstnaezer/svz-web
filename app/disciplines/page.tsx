b290a4b91028a8baa53a97c4b40397e0275579da
Ernst Naezer <p298892@DHGM121935.home>
2024-04-02 15:41:05 +0200

    Disciplines bijgewerkt

--- app/disciplines/page.tsx
+++ app/disciplines/page.tsx
@@ -11,19 +11,24 @@ const Geschiedenis = () => {
           volgende disciplines te beoefenen:
         </p>
         <ul>
-          <li>Luchtgeweer staand/knielend/liggend</li>
+          <li>Luchtgeweer staand/knielend</li>
           <li>Luchtpistool</li>
-          <li>Klein Kaliber geweer staand/knielend</li>
-          <li>Klein Kaliber Karabijn Klein</li>
-          <li>Kaliber Semi-automatisch Geweer staand/knielend/liggend Klein</li>
-          <li>Klein kaliber Vrij Geweer</li>
-          <li>Klein Kaliber pistool</li>
-          <li>Groot Kaliber geweer *</li>
-          <li>Militair geweer *</li>
-          <li>Precisie Geweer PG100*</li>
-          <li>Groot Kaliber pistool</li>
-          <li>Militair pistool</li>
+          <li>Klein Kaliber Pistool</li>
+          <li>Klein Kaliber Geweer staand/knielend/liggend</li>
+          <li>Klein Kaliber Karabijn staand/knielend/liggend</li>
+          <li>Klein Kaliber Semi-automatisch Geweer staand/knielend/liggend</li>
+          <li>
+            Klein Kaliber Semi-automatisch Karabijn staand/knielend/liggend
+          </li>
+          <li>Klein Kaliber Vrij Geweer</li>
+          <li>Groot Kaliber Pistool</li>
+          <li>Groot Kaliber Geweer *</li>
+          <li>Militair Pistool</li>
           <li>Service Pistol</li>
+          <li>Meesterkaart licht/zwaar</li>
+          <li>Militair geweer *</li>
+          <li>Precisie Geweer PG100 *</li>
+          <li>Dynamic Service Rifle *</li>
           <li>Historisch wapen (alleen Mariette of Colt)</li>
         </ul>
         <p>* op extern gehuurde banen</p>
